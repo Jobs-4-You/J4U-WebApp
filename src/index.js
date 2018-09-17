70a5813581c3fbbf1c093b55cbce92fa9da13714
JimZer <jimi.vaubien@protonmail.com>
2018-09-17 17:45:04 +0200

    WebApp boilerplate: React, Webpack

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import FormContainer from "./js/components/FormContainer";
+
+const wrapper = document.getElementById("app");
+
+ReactDOM.render(<FormContainer />, wrapper);
