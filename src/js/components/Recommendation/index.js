50b08dc95d74f44db63e1ae12dab2bd6ac593259
Lucas Rodrigues <lrodri@terpmail.umd.edu>
2019-05-14 16:30:03 +0200

    Added alpha/beta and previous job blocking to Recom

--- src/js/components/Recommendation/index.js
+++ src/js/components/Recommendation/index.js
@@ -30,6 +30,7 @@ const locationStyles = {
   })
 };
 
+
 const defaultLocation = [{ label: "Neuchâtel (NE)", value: "NE" }];
 
 function Recommendation() {
@@ -43,35 +44,66 @@ function Recommendation() {
                 Préférences de Recommandation Professionnelle
               </Typography>
               <br />
-              <AsyncSelect
-                cacheOptionscd
-                styles={selectStyles}
-                defaultInputValue={appContainer.state.oldJobLabel}
-                loadOptions={v =>
-                  recomContainer.handleSearch(v, errorContainer.displayError)
-                }
-                defaultOptions
-                onChange={appContainer.setOldJobValue}
-              />
-              <br />
-              <Typography id="label">
-                Importance de mon profil personnel :{" "}
-                {(appContainer.state.alpha / 100).toFixed(2)}{" "}
-              </Typography>
-              <StyledSlider
-                value={Math.round(appContainer.state.alpha)}
-                aria-labelledby="label"
-                onChange={appContainer.setAlpha}
-              />
-              <Typography id="label">
-                Importance de mon poste antérieur :{" "}
-                {(appContainer.state.beta / 100).toFixed(2)}
-              </Typography>
-              <StyledSlider
-                value={Math.round(appContainer.state.beta)}
-                aria-labelledby="label"
-                onChange={appContainer.setBeta}
-              />
+              <div style={{
+                display: appContainer.state.fixedOldJobValue ? "block" : "none",
+                margin: "1rem"
+              }}>
+                <Typography>Poste antérieur : {appContainer.state.oldJobLabel}</Typography>
+              </div>
+              <div style={{
+                  display: !appContainer.state.fixedOldJobValue ? "block" : "none"
+                }}>
+                <AsyncSelect
+                  cacheOptionscd
+                  styles={selectStyles}
+                  defaultInputValue={appContainer.state.oldJobLabel}
+                  loadOptions={v =>
+                    recomContainer.handleSearch(v, errorContainer.displayError)
+                  }
+                  defaultOptions
+                  onChange={appContainer.setOldJobValue}
+                  disabled={appContainer.state.fixedOldJobValue}
+                />
+                <br />
+              </div>
+              <div style={{
+                  display: appContainer.state.fixedAlphaBeta ? "block" : "none",
+                  margin: "1rem"
+                }}>
+                  <Typography>
+                    Importance de mon profil personnel : {Math.round(appContainer.state.alpha)}
+                  </Typography>
+                  <Typography>
+                    Importance de mon poste antérieur : {Math.round(appContainer.state.beta)}
+                  </Typography>
+              </div>
+
+              <div style={{
+                  display: !appContainer.state.fixedAlphaBeta ? "block" : "none"
+                }}>
+
+                <Typography id="label">
+                  Importance de mon profil personnel :{" "}
+                  {(appContainer.state.alpha / 100).toFixed(2)}{" "}
+                </Typography>
+                <StyledSlider
+                  value={Math.round(appContainer.state.alpha)}
+                  aria-labelledby="label"
+                  onChange={appContainer.setAlpha}
+                  disabled={appContainer.state.fixedAlphaBeta}
+                />
+                <Typography id="label">
+                  Importance de mon poste antérieur :{" "}
+                  {(appContainer.state.beta / 100).toFixed(2)}
+                </Typography>
+                <StyledSlider
+                  value={Math.round(appContainer.state.beta)}
+                  aria-labelledby="label"
+                  onChange={appContainer.setBeta}
+                  disabled={appContainer.state.fixedAlphaBeta}
+                />
+
+              </div>
               <AsyncSelect
                 cacheOptions
                 styles={locationStyles}
