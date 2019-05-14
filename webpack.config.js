3889656f991e4b6e3e4608fd175dd6c0c575c71c
JimZer <jimi.vaubien@protonmail.com>
2019-05-14 14:31:10 -0400

    .

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
     ]
   },
   plugins: [
-    new HtmlWebPackPlugin({§
+    new HtmlWebPackPlugin({
       template: "./src/index.html",
       filename: "./index.html"
     }),
