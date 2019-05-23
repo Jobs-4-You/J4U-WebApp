3602f42a9ef7a1a52a1ea54cdab7c04056ce65aa
Lucas Rodrigues <lrodri@terpmail.umd.edu>
2019-05-23 14:33:47 +0200

    Added query to admin page

--- src/js/components/Recommendation/RecomResults.js
+++ src/js/components/Recommendation/RecomResults.js
@@ -44,38 +44,43 @@ function OpenPosition({ recomContainer, i }) {
   try {
     const openPos  = recomContainer.state.openPositions[i];
     if (openPos) {
-      return openPos.map(
-        (job, i) =>
-        <ListItem button key={i}>
-          <ListItemText inset onClick={_ => recomContainer.setSelectedJob(recomContainer, job)}>
-              <Typography>
-                <strong>{job.jobAdvertisement.jobContent.jobDescriptions[0].title.toString()}</strong>
-              </Typography>
-              <Typography>
-                {job.jobAdvertisement.createdTime ? new Date(job.jobAdvertisement.createdTime).toLocaleDateString("fr-CH") : ""}
-              </Typography>
-              <span className="inline">
-                <strong>{job.jobAdvertisement.jobContent.company.name}</strong>
-              </span>
-              <JobBit>
-                {job.jobAdvertisement.jobContent.location ? (job.jobAdvertisement.jobContent.location.postalCode || "") + " " + (job.jobAdvertisement.jobContent.location.city  || "") + " " + (job.jobAdvertisement.jobContent.location.cantonCode || "") : ""}
-              </JobBit>
-              <JobBit>
-                {(job.jobAdvertisement.jobContent.employment.workloadPercentageMin && job.jobAdvertisement.jobContent.employment.workloadPercentageMin !== "100")  ? job.jobAdvertisement.jobContent.employment.workloadPercentageMin.toString() + "% - " : ""}
-                {job.jobAdvertisement.jobContent.employment.workloadPercentageMax + "%"}
-              </JobBit>
-              <JobBit>
-                {job.jobAdvertisement.jobContent.employment.immediately == true ? "Tout de suite" : (job.jobAdvertisement.jobContent.employment.startDate ? job.jobAdvertisement.jobContent.employment.startDate.toString() : "À convenir") }
-              </JobBit>
-              <JobBit>
-                {job.jobAdvertisement.jobContent.employment.permanent == true ? "Indeterminé" : "Durée limitée"}
-              </JobBit>
-              <Typography>
-                {job.jobAdvertisement.jobContent.jobDescriptions[0].description.length >= 280 ? job.jobAdvertisement.jobContent.jobDescriptions[0].description.substring(0, 279) + "..." : job.jobAdvertisement.jobContent.jobDescriptions[0].description.toString()}
-              </Typography>
-            </ListItemText>
-        </ListItem>
-      );
+      try {
+        return openPos.map(
+          (job, i) =>
+          <ListItem button key={i}>
+            <ListItemText inset onClick={_ => recomContainer.setSelectedJob(recomContainer, job)}>
+                <Typography>
+                  <strong>{job.jobAdvertisement.jobContent.jobDescriptions[0].title.toString()}</strong>
+                </Typography>
+                <Typography>
+                  {job.jobAdvertisement.createdTime ? new Date(job.jobAdvertisement.createdTime).toLocaleDateString("fr-CH") : ""}
+                </Typography>
+                <span className="inline">
+                  <strong>{job.jobAdvertisement.jobContent.company.name}</strong>
+                </span>
+                <JobBit>
+                  {job.jobAdvertisement.jobContent.location ? (job.jobAdvertisement.jobContent.location.postalCode || "") + " " + (job.jobAdvertisement.jobContent.location.city  || "") + " " + (job.jobAdvertisement.jobContent.location.cantonCode || "") : ""}
+                </JobBit>
+                <JobBit>
+                  {(job.jobAdvertisement.jobContent.employment.workloadPercentageMin && job.jobAdvertisement.jobContent.employment.workloadPercentageMin !== "100")  ? job.jobAdvertisement.jobContent.employment.workloadPercentageMin.toString() + "% - " : ""}
+                  {job.jobAdvertisement.jobContent.employment.workloadPercentageMax + "%"}
+                </JobBit>
+                <JobBit>
+                  {job.jobAdvertisement.jobContent.employment.immediately == true ? "Tout de suite" : (job.jobAdvertisement.jobContent.employment.startDate ? job.jobAdvertisement.jobContent.employment.startDate.toString() : "À convenir") }
+                </JobBit>
+                <JobBit>
+                  {job.jobAdvertisement.jobContent.employment.permanent == true ? "Indeterminé" : "Durée limitée"}
+                </JobBit>
+                <Typography>
+                  {job.jobAdvertisement.jobContent.jobDescriptions[0].description.length >= 280 ? job.jobAdvertisement.jobContent.jobDescriptions[0].description.substring(0, 279) + "..." : job.jobAdvertisement.jobContent.jobDescriptions[0].description.toString()}
+                </Typography>
+              </ListItemText>
+          </ListItem>
+        );
+        } catch (error) {
+          console.log(error);
+          return null;
+        }
     } else {
       return null;
     }
@@ -205,7 +210,7 @@ function JobDetail({ recomContainer }) {
                 </Typography>
                 <Typography paragraph={true}>
                   <JobBit>
-                    {(jobContent.location.postalCode || "")} {(jobContent.location.city || "")} ({(jobContent.location.cantonCode || "")})
+                    {(jobContent.location.postalCode || "")} {(jobContent.location.city || "")} {(jobContent.location.cantonCode || "")}
                   </JobBit>
                   <JobBit>
                     {(jobContent.employment.workloadPercentageMin && jobContent.employment.workloadPercentageMin !== "100")  ? jobContent.employment.workloadPercentageMin + "% - " : ""}
